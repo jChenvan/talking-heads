b276d43afc05d496321ae07e0b4a388c989bc899
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-07-14 16:12:06 -0400

    Add title

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,6 +5,7 @@ import useCharacter from '@/hooks/useCharacter';
 import useRealtimeChat from '@/hooks/useRealtimeChat';
 import cn from '@/utils/cn';
 import throttle from '@/utils/throttle';
+import Head from 'next/head';
 import { useCallback, useEffect, useRef } from 'react';
 
 export default function Chat() {
@@ -116,74 +117,78 @@ export default function Chat() {
     }
   },[canvas, canvasContainerRef]);
 
-  return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div className="bg-gray-900 p-6 rounded-xl flex gap-2">
-        <div ref={canvasContainerRef} className='bg-gray-950 rounded-lg w-[500px] h-[500px]'></div>
-        <div className="bg-gray-700 p-4 rounded-lg select-none">
-          <h1 className="mb-1.5">
-            Prompt
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                chatbot.setPrompt('');
-              }}
-              className="ml-2 p-1 bg-gray-900 rounded-sm active:bg-gray-400 cursor-pointer"
-            >
-              clear
-            </button>
-          </h1>
-          <textarea
-            rows={5}
-            className="w-[50ch] bg-gray-900 rounded-md resize-none p-2 focus:outline-0"
-            value={chatbot.prompt}
-            onChange={(e) => chatbot.setPrompt(e.target.value)}
-            placeholder="You are a..."
-          />
-          <h1 className="w-fit my-1.5">Voice</h1>
+  return (<>
+  <Head>
+    <title>Realtime Chat</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  </Head>
+  <div className="h-screen w-screen flex justify-center items-center">
+    <div className="bg-gray-900 p-6 rounded-xl flex gap-2">
+      <div ref={canvasContainerRef} className='bg-gray-950 rounded-lg w-[500px] h-[500px]'></div>
+      <div className="bg-gray-700 p-4 rounded-lg select-none">
+        <h1 className="mb-1.5">
+          Prompt
           <button
-            className="cursor-pointer"
-            onClick={() =>
-              chatbot.setVoice((prev) => {
-                if (prev === 'male') return 'female';
-                else return 'male';
-              })
-            }
-          >
-            <div className="flex bg-gray-900 rounded-sm w-fit">
-              <div
-                className={cn(
-                  'ml-2 mt-2 mb-2 rounded-sm w-[30px] h-[30px] flex justify-center items-center',
-                  chatbot.voice === 'male' && 'bg-blue-800'
-                )}
-              >
-                M
-              </div>
-              <div
-                className={cn(
-                  'm-2 rounded-sm w-[30px] h-[30px] flex justify-center items-center',
-                  chatbot.voice === 'female' && 'bg-pink-500'
-                )}
-              >
-                F
-              </div>
-            </div>
-          </button>
-          <button
-            className="block bg-blue-950 w-full py-2 rounded-lg mt-2 hover:bg-blue-600 active:bg-blue-300 cursor-pointer"
             onClick={(e) => {
               e.preventDefault();
-              if (chatbot.isSessionActive) {
-                chatbot.stopSession();
-              } else {
-                chatbot.startSession();
-              }
+              chatbot.setPrompt('');
             }}
+            className="ml-2 p-1 bg-gray-900 rounded-sm active:bg-gray-400 cursor-pointer"
           >
-            {chatbot.isSessionActive ? 'stop session' : 'begin session'}
+            clear
           </button>
-        </div>
+        </h1>
+        <textarea
+          rows={5}
+          className="w-[50ch] bg-gray-900 rounded-md resize-none p-2 focus:outline-0"
+          value={chatbot.prompt}
+          onChange={(e) => chatbot.setPrompt(e.target.value)}
+          placeholder="You are a..."
+        />
+        <h1 className="w-fit my-1.5">Voice</h1>
+        <button
+          className="cursor-pointer"
+          onClick={() =>
+            chatbot.setVoice((prev) => {
+              if (prev === 'male') return 'female';
+              else return 'male';
+            })
+          }
+        >
+          <div className="flex bg-gray-900 rounded-sm w-fit">
+            <div
+              className={cn(
+                'ml-2 mt-2 mb-2 rounded-sm w-[30px] h-[30px] flex justify-center items-center',
+                chatbot.voice === 'male' && 'bg-blue-800'
+              )}
+            >
+              M
+            </div>
+            <div
+              className={cn(
+                'm-2 rounded-sm w-[30px] h-[30px] flex justify-center items-center',
+                chatbot.voice === 'female' && 'bg-pink-500'
+              )}
+            >
+              F
+            </div>
+          </div>
+        </button>
+        <button
+          className="block bg-blue-950 w-full py-2 rounded-lg mt-2 hover:bg-blue-600 active:bg-blue-300 cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            if (chatbot.isSessionActive) {
+              chatbot.stopSession();
+            } else {
+              chatbot.startSession();
+            }
+          }}
+        >
+          {chatbot.isSessionActive ? 'stop session' : 'begin session'}
+        </button>
       </div>
     </div>
-  );
+  </div>
+  </>);
 }
